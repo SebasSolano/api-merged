22c7f8225a78ba57f25afcdd2d9c385232841a26
DesarrolloAlcaldiaMTR <desarrollo@monteria.gov.co>
2024-05-15 17:32:13 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,29 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import FileResponse, Response
-from firebase_admin import storage
+from flask import Flask, request, send_file, jsonify, Response
+from firebase_admin import storage, initialize_app, credentials
 from merged import merge_and_delete
 from orderFile import orderFile
 import os
-import firebase_admin
-from firebase_admin import credentials
 
+app = Flask(__name__)
+
+# Inicialización de Firebase
 cred = credentials.Certificate("./erp-mtrtest-18f0f398893a.json")
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'erp-mtrtest.appspot.com'
-})
-
-
-
-app = FastAPI()
+initialize_app(cred, {'storageBucket': 'erp-mtrtest.appspot.com'})
 
 def validate_and_create_folders():
     folders = ['temp_download', 'merged']
-    
     for folder in folders:
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-
-@app.get("/api/")
+@app.route("/api/")
 def index():
     return "API ON"
 
-
-'''
-    Con este link @/api/merged/{uid}-{id_con}-{id_info}
-    Se pasa uid(ID del usuario), id_con(ID del contrato),
-    id_info(ID de informes)
-'''
-@app.get("/api/merged/{uid}-{id_con}-{id_info}")
-async def download_file(uid: str,id_con: str,id_info: str ):
+@app.route("/api/merged/<string:uid>-<string:id_con>-<string:id_info>", methods=["GET"])
+def download_file(uid, id_con, id_info):
     if not uid:
-        raise HTTPException(status_code=400, detail="UID no válido")
+        return jsonify({"detail": "UID no válido"}), 400
 
     validate_and_create_folders()
     destination_folder = f"temp_download/{uid}/"
@@ -48,7 +34,6 @@ async def download_file(uid: str,id_con: str,id_info: str ):
 
     try:
         blobs = bucket.list_blobs(prefix=f"documentos/{uid}/")
-        
         for blob in blobs:
             file_name = os.path.basename(blob.name)
             blob.download_to_filename(os.path.join(destination_folder, file_name))
@@ -57,36 +42,27 @@ async def download_file(uid: str,id_con: str,id_info: str ):
         blob.download_to_filename(os.path.join(destination_folder, "RP.pdf"))
 
         blobs = bucket.list_blobs(prefix=f"documentos/2024/{id_con}/informes/{id_info}/anexos/")
-        
         for blob in blobs:
             file_name = os.path.basename(blob.name)
             blob.download_to_filename(os.path.join(destination_folder, file_name))
         
         orderFile(destination_folder)
         
-        if(merge_and_delete(uid, id_info)):
+        if merge_and_delete(uid, id_info):
             pdf_file_path = f"merged/merged-{id_info}.pdf"
-            pdf_content = open(pdf_file_path, 'rb').read()
-            response = Response(content=pdf_content, media_type='application/pdf')
-            response.headers["Content-Disposition"] = f'inline; filename="merged-{id_info}.pdf"'
-            return response
+            with open(pdf_file_path, 'rb') as pdf_file:
+                pdf_content = pdf_file.read()
+            return Response(pdf_content, mimetype='application/pdf', headers={"Content-Disposition": "inline"})
         else:
-            return {"error": f"There was an error with the UID identification: {uid}"}
+            return jsonify({"error": f"There was an error with the UID identification: {uid}"}), 500
     
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        return jsonify({"detail": str(e)}), 500
 
-
-
-'''
-    Con este link @/api/merged/deleted/{action}
-    Se pasa action(0 or 1) que se identifican como
-    False or True
-'''
-@app.get("/api/merged/deleted/{action}")
-def deleting_folder(action: int):
+@app.route("/api/merged/deleted/<int:action>", methods=["GET"])
+def deleting_folder(action):
     if action == 1:
-        directory = f"merged/"
+        directory = "merged/"
         pdf_files = [file for file in os.listdir(directory) if file.endswith('.pdf')]
         if len(pdf_files) < 1:
             print("There are not enough PDF files in the directory to merge.")
@@ -101,10 +77,12 @@ def deleting_folder(action: int):
                     print("Could not delete the file:", full_path, "Error:", e)
             if len(pdf_files) == 0:
                 return "All PDFs in the merged/ folder have been deleted."
-       
     elif action == 0:
         return "The real one, 0, doesn't do anything :)"
     else:
         return "ERROR: You can only enter 0 or 1."
 
-    
+if __name__ == "__main__":
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app.
+    app.run(host="127.0.0.1", port=8080, debug=True)
